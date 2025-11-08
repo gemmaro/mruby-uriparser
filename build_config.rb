296b4dea3579e3dfa728fc186d37a24111b256a6
gemmaro <gemmaro.dev@gmail.com>
2025-11-08 21:18:09 +0900

    Do not use compiler specific defines.

--- build_config.rb
+++ build_config.rb
@@ -29,7 +29,7 @@ MRuby::Build.new do |conf|
 
   if ENV['DEBUG'] == 'true'
     conf.enable_debug
-    conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+    conf.defines.concat(%w(MRB_ENABLE_DEBUG_HOOK))
     conf.gem core: 'mruby-bin-debugger'
   end
 end
