89b24e73bfed5694b79489c44f366b5b27acec82
gemmaro <gemmaro.dev@gmail.com>
2025-11-11 20:12:23 +0900

    Install Bear.

--- manifest.scm
+++ manifest.scm
@@ -1,8 +1,10 @@
 (use-modules (guix profiles)
              (gnu packages commencement)
              (gnu packages web)
+             (gnu packages build-tools)
              (gnu packages documentation))
 
 (packages->manifest (list gcc-toolchain
                           uriparser
-                          doxygen))
+                          doxygen
+                          bear))
